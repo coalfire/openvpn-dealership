4d51c4db37ec2cc43c3c86d1dcae187fba3c283c
Noah Birnel <nbirnel@gmail.com>
2016-08-24 15:23:51 -0700

    add newclient tests

--- clients.py
+++ clients.py
@@ -1,5 +1,13 @@
 class IpsSaturatedError(Exception): pass
 
+class DuplicateClientError(Exception): pass
+
+clients_dir = '/etc/openvpn/clients'
+conf = '/etc/openvpn/server.conf'
+
 def next_available_ip():
+    return 'foo'
+
+def new_client(name, ip, netmask):
     pass
 
--- tests/test_clients.py
+++ tests/test_clients.py
@@ -1,38 +1,65 @@
 import unittest
-from .context import clients
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+import clients
 
 class ThereAreNoClientsTest(unittest.TestCase):
-
+    
     def test(self):
         expected = '10.0.0.2'
-        actual = clients.next_available_ip()
-        self.assertEqual(expected, actual)
+        result = clients.next_available_ip()
+        self.assertEqual(expected, result)
 
 class ClientsHaveAnOpeningTest(unittest.TestCase):
 
     def test(self):
         expected = '10.0.0.3'
-        actual = clients.next_available_ip()
-        self.assertEqual(expected, actual)
+        result = clients.next_available_ip()
+        self.assertEqual(expected, result)
 
 class ClientsAreContiguousTest(unittest.TestCase):
 
     def test(self):
         expected = '10.0.0.5'
-        actual = clients.next_available_ip()
-        self.assertEqual(expected, actual)
+        result = clients.next_available_ip()
+        self.assertEqual(expected, result)
 
 class IpsRollOverOctetsTest(unittest.TestCase):
 
     def test(self):
         expected = '10.0.1.0'
-        actual = clients.next_available_ip()
-        self.assertEqual(expected, actual)
+        result = clients.next_available_ip()
+        self.assertEqual(expected, result)
 
 class ThereAreNoIpsAvailableTest(unittest.TestCase):
 
     def test(self):
         self.assertRaises(clients.IpsSaturatedError, clients.next_available_ip)
+
+class NewClientTest(unittest.TestCase):
+
+    def testDupClient(self):
+        expected = ['ifconfig_push 10.0.0.2 255.255.255.0']
+        ip = '10.0.0.2'
+        netmask = '255.255.255.0'
+        name = 'dummy'
+        result = clients.new_client(name, ip, netmask)
+        self.assertRaises(clients.DuplicateClientError, clients.new_client, name, ip, netmask)
+
+
+    def testNewClient(self):
+        expected = ['ifconfig_push 10.0.0.2 255.255.255.0']
+        ip = '10.0.0.2'
+        netmask = '255.255.255.0'
+        name = 'dummy'
+        result = clients.new_client(name, ip, netmask)
+        self.assertEqual(expected, result)
+
+    def tearDown(self):
+        #FIXME remove old client config
+        pass
 
 
 if __name__ == '__main__':
