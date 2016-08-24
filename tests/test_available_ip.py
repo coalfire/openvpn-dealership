3e588d8fa51c9354e0ebf36372e36670feb92c28
Noah Birnel <nbirnel@gmail.com>
2016-08-24 14:22:44 -0700

    flesh our unittests

--- tests/test_available_ip.py
+++ tests/test_available_ip.py
@@ -3,17 +3,35 @@ import unittest
 class ThereAreNoClientsTest(unittest.TestCase):
 
     def test(self):
-        self.assertEqual(0,1)
+        expected = '10.0.0.2'
+        actual = clients.next_available_ip()
+        self.assertEqual(expected, actual)
 
 class ClientsHaveAnOpeningTest(unittest.TestCase):
 
     def test(self):
-        self.assertEqual(0,1)
+        expected = '10.0.0.3'
+        actual = clients.next_available_ip()
+        self.assertEqual(expected, actual)
+
+class ClientsAreContiguousTest(unittest.TestCase):
+
+    def test(self):
+        expected = '10.0.0.5'
+        actual = clients.next_available_ip()
+        self.assertEqual(expected, actual)
+
+class IpsRollOverOctetsTest(unittest.TestCase):
+
+    def test(self):
+        expected = '10.0.1.0'
+        actual = clients.next_available_ip()
+        self.assertEqual(expected, actual)
 
 class ThereAreNoIpsAvailableTest(unittest.TestCase):
 
     def test(self):
-        self.assertEqual(0,1)
+        self.assertRaises(clients.IpsSaturatedError, clients.next_available_ip)
 
 
 if __name__ == '__main__':
